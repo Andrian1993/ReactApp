b0b9f4336c76e7e28908777af051f9067049e8b9
Unknown <andriantsoy@gmail.com>
2018-07-25 01:41:03 +0900

    added Navbar, Carousel

---
+++ src/js/IntroServices.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class IntroServices extends Component {
+  render() {
+    return (
+      
+    );
+  }
+}
+
+export default IntroServices;
--- src/js/Navbar.js
+++ src/js/Navbar.js
@@ -1,42 +1,31 @@
 import React, { Component } from 'react';
-import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import { Row, Grid, Col } from 'react-bootstrap';
+import '../css/Navbar.css';
 
 class NavBar extends Component {
   render() {
     return (
-        <Navbar inverse collapseOnSelect>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <a href="#brand">React-Bootstrap</a>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-          </Navbar.Header>
-          <Navbar.Collapse>
-            <Nav>
-              <NavItem eventKey={1} href="#">
-                Link
-              </NavItem>
-              <NavItem eventKey={2} href="#">
-                Link
-              </NavItem>
-              <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-                <MenuItem eventKey={3.1}>Action</MenuItem>
-                <MenuItem eventKey={3.2}>Another action</MenuItem>
-                <MenuItem eventKey={3.3}>Something else here</MenuItem>
-                <MenuItem divider />
-                <MenuItem eventKey={3.3}>Separated link</MenuItem>
-              </NavDropdown>
-            </Nav>
-            <Nav pullRight>
-              <NavItem eventKey={1} href="#">
-                Link Right
-              </NavItem>
-              <NavItem eventKey={2} href="#">
-                Link Right
-              </NavItem>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
+        <div>
+          <nav>
+            <Grid>
+              <Row className="show-grid">
+                <Col xs={12} md={4}>
+                  <a href="#">MODUSversus</a>
+                </Col>
+                <Col xs={6} md={8}>
+                  <a href="#">HOME</a>
+                  <a href="#">ABOUT</a>
+                  <a href="#">SERVICES</a>
+                  <a href="#">PORTFOLIO</a>
+                  <a href="#">BLOG</a>
+                  <a href="#">FEATURES</a>
+                  <a href="#">CONTACTS</a>
+                </Col>
+              </Row>
+            </Grid>
+          </nav>
+        </div>
+
     );
   }
 }
